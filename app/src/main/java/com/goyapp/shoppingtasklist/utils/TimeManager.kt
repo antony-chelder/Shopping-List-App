c741c5971a5553960c70592f746146eb5eff585c
Antony Chepel <71262438+antony-chelder@users.noreply.github.com>
2022-01-03 20:58:06 +0300

    Update TimeManager.kt

--- app/src/main/java/com/goyapp/shoppingtasklist/utils/TimeManager.kt
+++ app/src/main/java/com/goyapp/shoppingtasklist/utils/TimeManager.kt
@@ -6,20 +6,20 @@ import java.util.*
 
 object TimeManager {
      const val DEF_TIME_FORMAT = "hh:mm.ss - yyyy/MM/dd"
-      fun getCurrentTime() : String{ // Функция для получения текущего времени создания заметки, указываем определенный формат
-        val formatter = SimpleDateFormat(DEF_TIME_FORMAT, Locale.getDefault()) // Создали формат временеи, в каком виде оно будет
-        return formatter.format(Calendar.getInstance().time) // Возвращаем готовый формат с реальным временем
+      fun getCurrentTime() : String{ 
+        val formatter = SimpleDateFormat(DEF_TIME_FORMAT, Locale.getDefault()) 
+        return formatter.format(Calendar.getInstance().time) 
     }
 
 
-    fun getTimeFormat(time: String,defPreference : SharedPreferences) : String{ // Функция для того чтобы достать с сохранненого стринга формата времени данные
-       val defFormatter = SimpleDateFormat(DEF_TIME_FORMAT, Locale.getDefault()) // Получаем формат времени
-        val defDate = defFormatter.parse(time) // Разбиваем по отдельности данные с стринга
+    fun getTimeFormat(time: String,defPreference : SharedPreferences) : String{ 
+       val defFormatter = SimpleDateFormat(DEF_TIME_FORMAT, Locale.getDefault()) 
+        val defDate = defFormatter.parse(time) 
 
-        val newFormat = defPreference.getString("time_style_key", DEF_TIME_FORMAT) // Берем по ключу с sharedPreference какой формат был выбран, если нет, то значение по умолчанию
+        val newFormat = defPreference.getString("time_style_key", DEF_TIME_FORMAT) 
 
-        val newFormatter = SimpleDateFormat(newFormat, Locale.getDefault()) // Установка выбранного формата времени
+        val newFormatter = SimpleDateFormat(newFormat, Locale.getDefault()) 
 
-        return  if(defDate != null) newFormatter.format(defDate) else time // Возвращение обновленного формата времени
+        return  if(defDate != null) newFormatter.format(defDate) else time 
     }
 }
