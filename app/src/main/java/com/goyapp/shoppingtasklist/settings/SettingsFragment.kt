f2ee1c90de9cfb02c24c24a9a879cf7aca8fb16c
Antony Chepel <71262438+antony-chelder@users.noreply.github.com>
2022-01-03 20:55:20 +0300

    Update SettingsFragment.kt

--- app/src/main/java/com/goyapp/shoppingtasklist/settings/SettingsFragment.kt
+++ app/src/main/java/com/goyapp/shoppingtasklist/settings/SettingsFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 import com.goyapp.shoppingtasklist.R
 
-class SettingsFragment : PreferenceFragmentCompat() { // Создаем специальный фрагмент, который сможет сохранять выбранные настройки
+class SettingsFragment : PreferenceFragmentCompat() { 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_screen,rootKey)
 
