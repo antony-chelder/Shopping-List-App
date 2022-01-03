92dda7cd27d56237287df3ac237799b2c33c2a76
Antony Chepel <71262438+antony-chelder@users.noreply.github.com>
2022-01-03 20:39:21 +0300

    Update MainDatabase.kt

--- app/src/main/java/com/goyapp/shoppingtasklist/db/MainDatabase.kt
+++ app/src/main/java/com/goyapp/shoppingtasklist/db/MainDatabase.kt
@@ -10,21 +10,21 @@ import com.goyapp.shoppingtasklist.entities.ShopListItem
 import com.goyapp.shoppingtasklist.entities.ShopListName
 
 
-@Database(entities = [LibraryItem::class,NoteItem::class,ShopListItem::class,ShopListName::class],version = 1) // Перечислили все таблицы которые будут использоватся в базе данных, также версия, она меняется в случае изменений в одной из них
- abstract class MainDatabase : RoomDatabase() { // Класс создания базы данных
-    abstract fun getDao() :Dao // Подключение интерфейса Dao
+@Database(entities = [LibraryItem::class,NoteItem::class,ShopListItem::class,ShopListName::class],version = 1) 
+ abstract class MainDatabase : RoomDatabase() { 
+    abstract fun getDao() :Dao 
 
-  companion object{  // Возможность использовать функцию, переменные в другом классе без инициализации класса
-   @Volatile // ! Делает доступ к примеру инстанцию нашей базы данных для все потоков
+  companion object{  
+   @Volatile 
    private var INSTANCE : MainDatabase? = null
-   fun getDatabase(context:Context) : MainDatabase{ // Функция для получения инстанции базы данных
-      return INSTANCE ?: synchronized(this){// Когда мы будем пытатся получить инстанцию базы данных, она вернет если она создана или нет и хочет получить доступ другой класс или поток, то благодаря sync блокируется
-       val instance = Room.databaseBuilder(  // Проверка если где то на каком то потоке уже используется база данных,создание бд
+   fun getDatabase(context:Context) : MainDatabase{ 
+      return INSTANCE ?: synchronized(this){
+       val instance = Room.databaseBuilder(  
         context.applicationContext,
         MainDatabase::class.java,
         "shoppinglist.db"
        ).build()
-      instance  // Вернули instance
+      instance  
       }
 
    }
