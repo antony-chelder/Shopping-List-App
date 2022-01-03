1e31798c8e75244b04183d2891d9044ce5f1bf8e
Antony Chepel <71262438+antony-chelder@users.noreply.github.com>
2022-01-03 20:48:19 +0300

    Update UpdateListDialog.kt

--- app/src/main/java/com/goyapp/shoppingtasklist/dialogs/UpdateListDialog.kt
+++ app/src/main/java/com/goyapp/shoppingtasklist/dialogs/UpdateListDialog.kt
@@ -8,31 +8,31 @@ import com.goyapp.shoppingtasklist.databinding.EditItemDialogBinding
 import com.goyapp.shoppingtasklist.entities.ShopListItem
 
 object UpdateListDialog {
-    fun showdialog(context: Context,item : ShopListItem,listener:Listener){ // Передаем item, по которому будет обновление
-        var dialog : AlertDialog? = null  // Создали инстанцию диалога
-        val builder = AlertDialog.Builder(context) // Создан билдер
-        val binding = EditItemDialogBinding.inflate(LayoutInflater.from(context)) // Инициализировали binding класс где находится разметка диалога
+    fun showdialog(context: Context,item : ShopListItem,listener:Listener){ 
+        var dialog : AlertDialog? = null  
+        val builder = AlertDialog.Builder(context) 
+        val binding = EditItemDialogBinding.inflate(LayoutInflater.from(context)) 
 
-        builder.setView(binding.root) // Подключение разметки к билдеру
+        builder.setView(binding.root) 
 
          binding.apply {
-             edName.setText(item.name) // Значение которые уже были
+             edName.setText(item.name) 
              edDesc.setText(item.item_info)
-             if(item.item_type == 1) edDesc.visibility = View.GONE // Если редактирование libraryitem , то прячем edDesc, проверка благодаря типу
+             if(item.item_type == 1) edDesc.visibility = View.GONE 
           bUpdate.setOnClickListener {
               if(edName.text.toString().isNotEmpty()) {
-                  listener.onClick(item.copy(name = edName.text.toString(), item_info = edDesc.text.toString())) // Перезаписываем обновленные значение
+                  listener.onClick(item.copy(name = edName.text.toString(), item_info = edDesc.text.toString())) 
 
               }
               dialog?.dismiss()
           }
          }
-        dialog = builder.create() // Создали диалог
-        dialog.window?.setBackgroundDrawable(null) // Убираем ненужный фон помимо нашего
-        dialog.show() // Показываем диалог
+        dialog = builder.create() 
+        dialog.window?.setBackgroundDrawable(null) 
+        dialog.show() 
 
     }
-    interface Listener{ // Создание интерфейса, чтобы передать сохраненное название листа на Activity
+    interface Listener{ 
         fun onClick(item:ShopListItem)
 
     }
